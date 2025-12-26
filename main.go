d085d45004d05477efe3f07c2591a372a551a706
Torneira Super Sayajin <v.emanoel.moraes@gmail.com>
2025-12-26 15:59:52 -0300

    Update main.go

--- main.go
+++ main.go
@@ -30,7 +30,6 @@ func (p Password) Prompt() ([]byte, error) {
 		return nil, err
 	}
 	defer term.Restore(fd, state)
-
 	ans := make([]byte, p.MinLen)
 	buf := make([]byte, 1)
 	for {
@@ -38,17 +37,18 @@ func (p Password) Prompt() ([]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		if buf[0] == 13 {
+		b := buf[0]
+		if b == 13 {
 			break
 		}
-		if buf[0] == 127 || buf[0] == 8 {
+		if b == 127 || b == 8 {
 			if len(ans) > 0 {
 				ans = ans[:len(ans)-1]
 				fmt.Print("\b \b")
 			}
 			continue
 		}
-		if buf[0] == 3 || buf[0] == 24 || buf[0] == 27 {
+		if b == 3 || b == 24 || b == 27 {
 			if p.Skippable {
 				return nil, nil
 			}
@@ -57,7 +57,7 @@ func (p Password) Prompt() ([]byte, error) {
 		if p.Mask != 0 {
 			fmt.Print(string(p.Mask))
 		}
-		ans = append(ans, buf[0])
+		ans = append(ans, b)
 	}
 	return ans, nil
 }
