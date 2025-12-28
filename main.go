6f8ea8332eeda7ac6b9495bbf0502204d05a0abb
Torneira Super Sayajin <v.emanoel.moraes@gmail.com>
2025-12-27 21:12:02 -0300

    Update main.go

--- main.go
+++ main.go
@@ -4,24 +4,28 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/term"
 )
 
 var (
-	ErrOperationCanceled    = errors.New("Operation was canceled by the user")
 	ErrOperationInterrupted = errors.New("Operation was interrupted by the user")
+	ErrOperationCanceled    = errors.New("Operation was canceled by the user")
 	ErrNotTTY               = errors.New("The input device is not a TTY")
 )
 
 type RenderConfig struct {
+	PromptPrefix         rune
+	AnsweredPromptPrefix rune
 }
 
 type Password struct {
-	Message   string
-	Mask      rune
-	Skippable bool
-	RenderCfg any
+	Msg                    string
+	StartsVisible          bool
+	EnableVisibilityToggle bool
+	Mask                   rune
+	AllowSkip              bool
 }
 
 func (p *Password) SetMask(mask rune) {
@@ -38,37 +42,56 @@ func (p Password) Prompt() ([]byte, error) {
 		return nil, err
 	}
 	defer term.Restore(fd, state)
-	fmt.Print(p.Message)
+	fmt.Print(p.Msg)
 	ans := make([]byte, 0)
-	buf := make([]byte, 1)
+	buf := make([]byte, 6)
+	visible := p.StartsVisible
 	for {
-		_, err := os.Stdin.Read(buf)
+		n, err := os.Stdin.Read(buf)
 		if err != nil {
 			return nil, err
+		}
+		if n != 1 {
+			continue
 		}
 		b := buf[0]
 		if b == 13 {
 			fmt.Print("\r\n")
 			return ans, nil
 		}
-		if b == 8 || b == 127 {
-			if len(ans) > 0 {
-				ans = ans[:len(ans)-1]
-				fmt.Print("\b \b")
-			}
-			continue
-		}
 		if b == 3 || b == 27 {
-			fmt.Print("\033[0;31mskipped")
-			if p.Skippable {
+			if p.AllowSkip {
 				return nil, nil
 			}
+			fmt.Print("\r\n")
 			if b == 27 {
 				return nil, ErrOperationCanceled
 			}
 			return nil, ErrOperationInterrupted
 		}
-		if p.Mask != 0 {
+		if b == 8 || b == 127 {
+			if len(ans) > 0 {
+				fmt.Print("\b \b")
+				ans = ans[:len(ans)-1]
+			}
+			continue
+		}
+		if b == 18 {
+			if p.EnableVisibilityToggle {
+				fmt.Print(strings.Repeat("\b \b", len(ans)))
+				if visible {
+					visible = false
+					fmt.Print(strings.Repeat(string(p.Mask), len(ans)))
+				} else {
+					visible = true
+					fmt.Print(string(ans))
+				}
+			}
+			continue
+		}
+		if visible {
+			fmt.Print(string(b))
+		} else {
 			fmt.Print(string(p.Mask))
 		}
 		ans = append(ans, b)
